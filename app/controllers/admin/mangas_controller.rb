0781e224eda2d1d89b7283942de327ae65fd2a46
omochi8311 <omochi.1126.tmc@gmail.com>
2022-07-16 08:43:31 +0000

    [Update]ransack導入

--- app/controllers/admin/mangas_controller.rb
+++ app/controllers/admin/mangas_controller.rb
@@ -1,2 +1,25 @@
 class Admin::MangasController < ApplicationController
+  before_action :search
+
+  def new
+    if params[:keyword]
+        @mangas = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+    end
+      @manga = Manga.new
+  end
+
+  def search
+    @q = Manga.ransack(params[:q])
+  end
+
+  def index
+    # distinct: trueは重複したデータを除外
+    @posts = @q.result(distinct: true)
+  end
+
+  private
+
+  def search_params
+    params.require(:q).permit!
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,13 +23,13 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+
   scope module: :public do
     resources :mangas
     resources :posts
     get '/requests/completion' => 'requests#completion'
     get '/requests' => 'requests#new'
     post '/requests' => 'requests#create'
-    get '/posts/search' => 'posts#search'
     get '/customers/my_page' => 'customers#show'
     get '/customers/index' => 'customers#index'
     get '/customers/withdrawal' => 'customers#withdrawal'
@@ -40,10 +40,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :homes
-    resources :posts
     resources :mangas
     resources :genres
     resources :customers
     resources :requests
+    resources :posts do
+    collection do
+      get 'search'
+    end
+  end
   end
 end
