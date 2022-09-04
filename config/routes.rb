9f9b7e7fd1ffcd19cc0b4b322a595b0d41a530ae
omochi8311 <omochi.1126.tmc@gmail.com>
2022-09-04 08:35:56 +0000

    [Update]評価機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   get 'admin' => 'admin/requests#top'
 
 
-  # 顧客用
-  # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -16,17 +14,14 @@ Rails.application.routes.draw do
     post 'customer/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
 
-  # 管理者用
-  # URL /admin/sign_in ...
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
   scope module: :public do
     resources :mangas
-    #resources :posts
     get '/posts', to: 'posts#index', as: "posts"
     get '/mangas/:manga_id/post/new', to: 'posts#new', as: "manga_post_new"
     post '/mangas/:manga_id/post', to: 'posts#create', as: "manga_post_create"
