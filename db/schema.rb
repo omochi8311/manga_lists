2b821cae1f6db96686fc5bcf554d462ed4c031f7
omochi8311 <omochi.1126.tmc@gmail.com>
2022-09-12 08:11:57 +0000

    [Update]migrate修正

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 2022_07_11_123341) do
     t.string "author", null: false
     t.string "rakuten", null: false
     t.integer "isbn"
-    t.string "rakuten_image_url"
+    t.text "rakuten_image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
