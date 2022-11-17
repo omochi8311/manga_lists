83c8f08a21164640cad1121408213cc127d4ff40
omochi8311 <omochi.1126.tmc@gmail.com>
2022-11-17 10:22:15 +0000

    [Update]修正

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,6 +1,8 @@
 RakutenWebService.configure do |c|
   # (必須) アプリケーションID
-  c.application_id = ENV['RWS_APPLICATION_ID']
+  c.application_id = '1015611249791323364'
+
   # (任意) 楽天アフィリエイトID
-  c.affiliate_id = ENV['RWS_AFFILIATION_ID']
+  c.affiliate_id = '2981d6b2.d288edc3.2981d6b3.fd0a92d8'
+
 end
